d5b6fe2274588c2b39ec2262d45026af391369a6
Alan <dasilva.alan@outlook.com>
2021-12-18 16:04:21 +0200

    8.8

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,11 +1,24 @@
-  
-import React from 'react'
+import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`;
 
 const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS);
   if (!props.show) {
-    return null
+    return null;
   }
-  const authors = []
+  if (result.loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
@@ -14,25 +27,20 @@ const Authors = (props) => {
         <tbody>
           <tr>
             <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
+            <th>born</th>
+            <th>books</th>
           </tr>
-          {authors.map(a =>
+          {result.data.allAuthors.map((a) => (
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
               <td>{a.bookCount}</td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
-
     </div>
-  )
-}
+  );
+};
 
-export default Authors
+export default Authors;
